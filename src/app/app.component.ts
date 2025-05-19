0b9537e27f9e038fbb42fd28643a27500704da0d
jaicabher1 <jaicabher1@alum.us.es>
2025-05-19 17:09:09 +0200

    login in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,23 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MenuLateralComponent } from "./menu-lateral/menu-lateral.component";
 import { CursosComponent } from "./cursos/cursos/cursos.component";
+import { LoginComponent } from "./login/login.component";
+import { AuthService } from './auth.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MenuLateralComponent, CursosComponent],
+  imports: [RouterOutlet, MenuLateralComponent, CursosComponent, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  isLoggedIn = false;
+
+  constructor(private auth: AuthService) {}
+
+  ngOnInit() {
+    this.auth.isLoggedIn.subscribe(val => {
+      this.isLoggedIn = val;
+    });
+  }
 }
